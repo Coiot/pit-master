d896efbe299c94abe85743d5bfe6e57d38162169
Isaac Volpe <isaac.volpe@gmail.com>
2020-07-27 16:19:27 -0500

    Functions Fix

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -3,7 +3,7 @@
 
 require("dotenv").config();
 const axios = require("axios");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY),
+const stripe = require("stripe")(process.env.STRIPE_PUBLIC_KEY),
     headers = {
         'Content-Type': 'application/json',
         "Access-Control-Allow-Origin": "*",
