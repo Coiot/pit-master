2f42d794e51d0dd225f92baeecf27c24d5cf2a04
Isaac Volpe <isaac.volpe@gmail.com>
2020-07-29 04:12:21 -0500

    Functions Fix

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -57,9 +57,7 @@ exports.handler = async (event, context) => {
         const paymentIntent = await stripe.paymentIntents.create({
             currency: "usd",
             amount: amount,
-            description: JSON.stringify({
-                data.items
-            })
+            description: "Order from JMBBQ"
         });
 
         // Send the client_secret to the client
