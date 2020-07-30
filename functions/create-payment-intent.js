1344ccc6c93dfaee81699bb9372bbe081d110a7e
Isaac Volpe <isaac.volpe@gmail.com>
2020-07-29 22:46:23 -0500

    Email Descriptions

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -57,7 +57,7 @@ exports.handler = async (event, context) => {
         const paymentIntent = await stripe.paymentIntents.create({
             currency: "usd",
             amount: amount,
-            description: JSON.stringify({ data.items, })
+            description: JSON.stringify({ data.items })
         });
 
         // Send the client_secret to the client
